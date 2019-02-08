418920b94c858c8775be620bdd65a6e8bb7d5a04
ChaoChongTing <47215677+ChaoChongTing@users.noreply.github.com>
2019-02-08 12:55:57 +0800

    Add files via upload

---
+++ src/app/adminstuff/adminstuff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminstuff',
+  templateUrl: './adminstuff.component.html',
+  styleUrls: ['./adminstuff.component.css']
+})
+export class AdminstuffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
